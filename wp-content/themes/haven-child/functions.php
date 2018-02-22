a6fc700fa8ba8f15cb281d3a169110dbbdda4c57
David Pollard <pollardld@gmail.com>
2018-02-21 17:44:37 -0800

    Allow SVG uploads via wordpress

--- wp-content/themes/haven-child/functions.php
+++ wp-content/themes/haven-child/functions.php
@@ -6,3 +6,10 @@ function the_core_theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
 add_action( 'wp_enqueue_scripts', 'the_core_theme_enqueue_styles' );
+
+// Allow SVGs
+function cc_mime_types($mimes) {
+  $mimes['svg'] = 'image/svg+xml';
+  return $mimes;
+}
+add_filter('upload_mimes', 'cc_mime_types');
